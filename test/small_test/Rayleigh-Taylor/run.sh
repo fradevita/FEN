1804aabce97e3f8fdfe4ea6af6414766857883ad
Francesco De Vita <fnc.devita@gmail.com>
2022-11-24 13:20:41 +0100

    fix: fix small test cases
    Fixed capillary and Rayleigh-Taylor test case not createing the ouput directory.

--- test/small_test/Rayleigh-Taylor/run.sh
+++ test/small_test/Rayleigh-Taylor/run.sh
@@ -1,5 +1,7 @@
 echo "Running Rayleigh-Taylor instability test case..."
 
+mkdir -p data
+
 make SOURCE=Rayleigh_Taylor > compilation_log 2> compilation_warning
 
 mpirun -n 1 ./code.e > log 2> error.err
