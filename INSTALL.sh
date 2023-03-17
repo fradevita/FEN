5e96bd5b7db592401790ca8b3fc68dd50ab63a2f
Francesco De Vita <fdv@LAPTOP-JLQJ2JTF>
2023-03-17 21:55:00 +0100

    INSTALL script now checks if FFTW3_DIR and _2DECOMP_DIR exist before install

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,11 +2,16 @@
 
 # Define environmental variables
 # (NOTE: considering to add this line to your bashrc to avoid exporting every time)
-export FEN_DIR=/home/devita/prove/install_script/REO
-export _2DECOMP_DIR=/home/devita/prove/install_script/2decomp_fft
-export FFTW3_DIR=/home/devita/prove/install_script/fftw3
+export FEN_DIR=??
+export _2DECOMP_DIR=??
+export FFTW3_DIR=??
 
 # Install FFTW3 library
+if [ ! -d "$FFTW3_DIR" ];
+then
+	echo "Please provide a correct path to the variable FFTW3_DIR"
+	exit 1
+fi
 cd $FFTW3_DIR
 wget http://www.fftw.org/fftw-3.3.10.tar.gz
 tar -zxvf fftw-3.3.10.tar.gz
@@ -16,6 +21,11 @@ make
 make install
 
 # Install 2decomp&FFT library
+if [ ! -d "${_2DECOMP_DIR}" ];
+then
+        echo "Please provide a correct path to the variable _2DECOMP_DIR"
+        exit 1
+fi
 git clone https://github.com/numericalalgorithmsgroup/2decomp_fft.git ${_2DECOMP_DIR}
 cd ${_2DECOMP_DIR}
 sed -i '13s/OPTIONS=/OPTIONS= -DDOUBLE_PREC/' src/Makefile.inc
